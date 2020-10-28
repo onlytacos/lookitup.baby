13a6e7c472ad8997eaab6b2a0a3f632feaecf3ae
Brandon Dimcheff <brandon@dimcheff.com>
2020-10-28 19:47:41 -0400

    add /kubecon endpoint

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 		port = "8080"
 	}
 	log.Printf("HONKS ONLINE ON PORT %v", port)
+	http.Handle("/kubecon", http.RedirectHandler("https://www.youtube.com/watch?v=ZY4kTDCsJ44", 302))
 	http.Handle("/", http.RedirectHandler("https://twitter.com/IanColdwater/status/1292895288546545666", 302))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
