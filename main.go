a57db76ef3289bf03209b6618f4cf79de7cef1b3
Brandon Dimcheff <brandon@dimcheff.com>
2020-11-13 10:54:32 -0500

    my computer is somebody else's cloud

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 		port = "8080"
 	}
 	log.Printf("HONKS ONLINE ON PORT %v", port)
+	http.Handle("/cloud", http.RedirectHandler("https://twitter.com/IanColdwater/status/1327273143652798464", 302))
 	http.Handle("/kubecon", http.RedirectHandler("https://www.youtube.com/watch?v=ZY4kTDCsJ44", 302))
 	http.Handle("/", http.RedirectHandler("https://twitter.com/IanColdwater/status/1292895288546545666", 302))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
