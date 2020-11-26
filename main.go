18f2d52631320a10c361b64d6acbcf8181ec1d76
Brandon Dimcheff <brandon@dimcheff.com>
2020-11-26 11:19:26 -0500

    /pop

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 		port = "8080"
 	}
 	log.Printf("HONKS ONLINE ON PORT %v", port)
+	http.Handle("/pop", http.RedirectHandler("https://www.youtube.com/watch?v=EDUy3Y_w9Tk", 302))
 	http.Handle("/cloud", http.RedirectHandler("https://twitter.com/IanColdwater/status/1327350827078406144", 302))
 	http.Handle("/kubecon", http.RedirectHandler("https://www.youtube.com/watch?v=ZY4kTDCsJ44", 302))
 	http.Handle("/", http.RedirectHandler("https://twitter.com/IanColdwater/status/1292895288546545666", 302))
