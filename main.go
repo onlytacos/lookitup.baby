5222dd40d71eb52ba44f463b6453460f60716888
Brandon Dimcheff <brandon@dimcheff.com>
2020-12-31 20:10:28 -0500

    Add defcon
    DEFCON awards

--- main.go
+++ main.go
@@ -19,8 +19,10 @@ func main() {
 	http.Handle("/pop", http.RedirectHandler("https://www.youtube.com/watch?v=EDUy3Y_w9Tk", 302))
 	http.Handle("/cloud", http.RedirectHandler("https://twitter.com/IanColdwater/status/1327350827078406144", 302))
 	http.Handle("/kubecon", http.RedirectHandler("https://www.youtube.com/watch?v=ZY4kTDCsJ44", 302))
-	http.Handle("/", http.RedirectHandler("https://twitter.com/IanColdwater/status/1292895288546545666", 302))
 	http.Handle("/betty", http.RedirectHandler("https://twitter.com/BettyJunod/status/1109186194959552513", 302))
+	http.Handle("/defcon", http.RedirectHandler("https://twitter.com/fox0x01/status/1344775712511819778", 302))
+	http.Handle("/", http.RedirectHandler("https://twitter.com/IanColdwater/status/1292895288546545666", 302))
+
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
